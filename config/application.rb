b6d9fe9968a21bdf16f471730e6fca7cfca7a456
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-07-25 13:23:19 -0300

    tentando adicionar a config direto no application

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module FileProjectS3
   class Application < Rails::Application
+
+    Aws.config.update({
+      region: 'us-east-1',
+      credentials: Aws::Credentials.new('AKIAIINULSJCBNLPUA7A', 'eHUZihH3KezvjcjLx0cfqOxL3B60Zqrw9lYZZ172'),
+      #:s3 => { :region => 'us-east-2' }
+    })
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
