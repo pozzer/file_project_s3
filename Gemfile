c8d86e1a43aeea2baf6cf32798cdd2b53c332b1c
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-07-27 14:02:23 -0300

    Adicionando algumas funções e arrumando design

--- Gemfile
+++ Gemfile
@@ -23,7 +23,9 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 
-gem 'aws-sdk'
+gem 'aws-sdk', '~> 2'
+
+gem "font-awesome-rails"
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/controllers/tree_controller.rb
+++ app/controllers/tree_controller.rb
@@ -1,31 +1,45 @@
+require 'aws-sdk'
+
 class TreeController  < ApplicationController
-  require 'aws-sdk'
+
   before_action :get_s3
 
   def index
     #buckets = list.buckets
-    @list_objects = @s3.list_objects({bucket: @bucket_name, delimiter: '/'})
+
+    @list_objects = @s3_client.list_objects({bucket: @bucket_name, delimiter: '/'})
+
   end
 
   def show
-    @list_objects = @s3.list_objects({bucket: @bucket_name, prefix:"#{params[:id]}/", delimiter: "/"})
+    @list_objects = @s3_client.list_objects({bucket: @bucket_name, prefix:"#{params[:id]}/", delimiter: "/"})
     render :index
   end
 
   def download
     key = params[:key]
     file_name = key.split("/").last
-    object = @s3.get_object({bucket:@bucket_name, key: key})
+    object = @s3_client.get_object({bucket:@bucket_name, key: key})
     send_data object.body.read,  :filename => file_name
   end
 
   private
     def get_s3
-      get_bucket
-      @s3 = Aws::S3::Client.new
+      set_bucket_name
+      @s3 = Aws::S3::Resource.new
+      @s3_client = Aws::S3::Client.new
+      @bucket = @s3.bucket(@bucket_name)
     end
 
-    def get_bucket
+    def set_bucket_name
       @bucket_name = 'mead-productions'
+    end
+
+    def new_bucket
+      bucket = @s3.buckets.create('name', :acl => :public_read)
+    end
+
+    def new_folder
+      @s3_client.put_object(bucket: @bucket_name, key: "teste/.keep")
     end
 end
